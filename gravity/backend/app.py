0a5c5e4e8676cb70dacf42b844f9e1c7ec123139
Hariprakash3131 <168099983+Hariprakash3131@users.noreply.github.com>
2025-07-06 14:29:12 +0530

    Update app.py

--- gravity/backend/app.py
+++ gravity/backend/app.py
@@ -17,12 +17,12 @@ CORS(app)
 
 # --- Database Connection Configuration ---
 # Load configuration from environment variables
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['MYSQL_SSL'] = {'ca': '/etc/ssl/cert.pem'}
+app.config['MYSQL_SSL'] = {'ca': 'pscale-ca-cert.pem'}
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
